3e028deaa27d57bccf0a5a71a7a55910ca96f11e
Dmitry V. Luciv <dluciv@dluciv.name>
2026-04-17 08:56:07 +0300

    Tidy

---
+++ tidy.sh
@@ -0,0 +1,14 @@
+CHECKS='-*,clang-analyzer-*,performance-*,bugprone-*'
+FILES=$(git ls-files '*.cpp' '*.cxx' '*.cc' | tr '\n' ' ')
+
+if [[ -z "$FILES" ]]; then
+  echo "No source files to analyze."
+  exit 0
+fi
+
+set -eo pipefail
+
+for f in $FILES; do
+  echo "Running clang-tidy on $f"
+  clang-tidy -p . -checks="$CHECKS" "$f" --quiet 2>&1
+done
