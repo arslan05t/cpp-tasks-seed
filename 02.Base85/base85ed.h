b77adaae5f025a21c1ca77fc1e3c530358073b4e
Arslan Tolegenov <arslantolegenov08@gmail.com>
2026-06-08 20:08:09 +0300

    Update base85ed.h

--- 02.Base85/base85ed.h
+++ 02.Base85/base85ed.h
@@ -1,20 +1,16 @@
 #ifndef __BASE85ED__
 #define __BASE85ED__
-
 #include <vector>
 #include <cstdint>
-
 namespace base85
 {
-    /**
-     * @brief Encodes bytes into a Base85.
-     */
-    std::vector<uint8_t> encode(std::vector<uint8_t> const &bytes);
-
-    /**
-     * @brief Decodes Base85 to bytes.
-     */
-    std::vector<uint8_t> decode(std::vector<uint8_t> const &b85str);
+/**
+ * @brief Encodes bytes into a Base85.
+ */
+std::vector<uint8_t> encode(std::vector<uint8_t> const &bytes);
+/**
+ * @brief Decodes Base85 to bytes.
+ */
+std::vector<uint8_t> decode(std::vector<uint8_t> const &b85str);
 } // namespace base85
-
 #endif // __BASE85ED__
